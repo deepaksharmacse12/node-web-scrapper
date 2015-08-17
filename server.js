aac87c30c775463525b3969002cb35d5671c87b9
Deepak Sharma <deepak.sharma.cse12@itbhu.ac.in>
2015-08-17 14:14:53 +0530

    update

--- server.js
+++ server.js
@@ -5,25 +5,48 @@ var cheerio = require('cheerio');
 var app     = express();
 
 app.get('/scrape', function(req, res){
-	// Let's scrape Anchorman 2
 	url = 'http://www.imdb.com/chart/top';
 
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
-			var json = {movies : []};
+			var json = {
+							title : [],
+							rating  : [],
+							url : []
+					   };
+			var output = {
+							movies : []
+						 };
 			
 			$('.titleColumn a').each(function(){
-				json.movies.push($(this).text());
+				json.title.push($(this).text());
 			});
 			
-		};
-
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        	console.log('File successfully written! - Check your project directory for the output.json file');
-        })
-
-        res.send('Check your console!')
+			$('.ratingColumn.imdbRating').each(function(){
+				json.rating.push($(this).children().text());
+			});
+			
+			$('.posterColumn a').each(function(){
+				json.url.push($(this).children().attr('src'));
+			});
+			
+			for(var i=0; i<json.title.length; i++){
+				output.movies[i] = {
+					title : json.title[i],
+					rating : json.rating[i],
+					url : json.url[i]
+				}
+			}
+			fs.writeFile('output.json', JSON.stringify(output, null, 4), function(err){
+		    	console.log('File successfully written! - Check your project directory for the output.json file');
+		    })
+		
+		    res.send('Check your console!')			
+		}
+		else {
+			console.log("Network Error, please try again later")
+		}
 	})
 })
 
